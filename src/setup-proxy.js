2cd0da596633810a103b5c56c4011859b34f08db
Ladislav Prskavec <ladislav@prskavec.net>
2019-04-28 09:25:40 +0200

    feat: add toxic functions

--- src/setup-proxy.js
+++ src/setup-proxy.js
@@ -1,6 +1,4 @@
-"use strict";
 const toxiproxyClient = require("toxiproxy-node-client");
-
 const toxiproxy = new toxiproxyClient.Toxiproxy("http://localhost:8474");
 
 const proxyBody = {
@@ -9,4 +7,28 @@ const proxyBody = {
   upstream: "172.28.0.10:6379"
 };
 
-toxiproxy.createProxy(proxyBody);
+function createProxy() {
+  toxiproxy.createProxy(proxyBody);
+}
+// Latency, Jitter value in ms
+function addLatency(proxy, latencyValue, jitterValue) {
+  const toxicBody = {
+    attributes: { latency: latencyValue, jitter: jitterValue },
+    type: "latency"
+  };
+  proxy.addToxic(new toxiproxyClient.Toxic(proxy, toxicBody));
+}
+// Latency value in ms, if 0 the connection won't close,
+function addTimeout(proxy, timeoutValue) {
+  const toxicBody = {
+    attributes: { timeout: timeoutValue },
+    type: "timeout"
+  };
+  proxy.addToxic(new toxiproxyClient.Toxic(proxy, toxicBody));
+}
+
+module.exports = {
+  createProxy: createProxy,
+  addLatency: addLatency,
+  addTimeout: addTimeout
+}
