3674b4ddeea139bd95dc5c835b9015670d0ff082
Ladislav Prskavec <ladislav@prskavec.net>
2019-04-28 17:23:48 +0200

    feat: add Prettier

--- src/setup-proxy.js
+++ src/setup-proxy.js
@@ -1,34 +1,38 @@
-const toxiproxyClient = require("toxiproxy-node-client");
-const toxiproxy = new toxiproxyClient.Toxiproxy("http://localhost:8474");
+const toxiproxyClient = require ('toxiproxy-node-client');
+const toxiproxy = new toxiproxyClient.Toxiproxy ('http://localhost:8474');
 
 const proxyBody = {
-  listen: "0.0.0.0:6666",
-  name: "redis1",
-  upstream: "172.28.0.10:6379"
+  listen: '0.0.0.0:6666',
+  name: 'redis1',
+  upstream: '172.28.0.10:6379',
 };
 
-function createProxy() {
-  toxiproxy.createProxy(proxyBody);
+function createProxy (proxyConfig = null) {
+  if (proxyConfig === null) {
+    toxiproxy.createProxy (proxyBody);
+  } else {
+    toxiproxy.createProxy (proxyConfig);
+  }
 }
 // Latency, Jitter value in ms
-function addLatency(proxy, latencyValue, jitterValue) {
+function addLatency (proxy, latencyValue, jitterValue) {
   const toxicBody = {
-    attributes: { latency: latencyValue, jitter: jitterValue },
-    type: "latency"
+    attributes: {latency: latencyValue, jitter: jitterValue},
+    type: 'latency',
   };
-  proxy.addToxic(new toxiproxyClient.Toxic(proxy, toxicBody));
+  proxy.addToxic (new toxiproxyClient.Toxic (proxy, toxicBody));
 }
 // Latency value in ms, if 0 the connection won't close,
-function addTimeout(proxy, timeoutValue) {
+function addTimeout (proxy, timeoutValue) {
   const toxicBody = {
-    attributes: { timeout: timeoutValue },
-    type: "timeout"
+    attributes: {timeout: timeoutValue},
+    type: 'timeout',
   };
-  proxy.addToxic(new toxiproxyClient.Toxic(proxy, toxicBody));
+  proxy.addToxic (new toxiproxyClient.Toxic (proxy, toxicBody));
 }
 
 module.exports = {
   createProxy: createProxy,
   addLatency: addLatency,
-  addTimeout: addTimeout
-}
+  addTimeout: addTimeout,
+};
