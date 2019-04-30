90b140bbccd7711218d1420921ac4c8c975557e3
Ladislav Prskavec <ladislav@prskavec.net>
2019-04-30 11:04:19 +0200

    fix: running test in docker

--- src/setup-proxy.js
+++ src/setup-proxy.js
@@ -1,39 +1,36 @@
-const toxiproxyClient = require ('toxiproxy-node-client');
-const toxiproxy = new toxiproxyClient.Toxiproxy (process.env.TOXIPROXY_URL + ":8474");
+const toxiproxyClient = require('toxiproxy-node-client')
+const toxiproxy = new toxiproxyClient.Toxiproxy(
+  process.env.TOXIPROXY_URL + ':8474'
+)
 
 const proxyBody = {
   listen: '0.0.0.0:6666',
   name: 'redis1',
   upstream: '172.28.0.10:6379',
-};
+}
 
-
-function createProxy (proxyConfig = null) {
-  if (proxyConfig && toxiproxy.get(proxyConfig.name)) {
-    return toxiproxy.get(proxyBody.name)
+function createProxy(proxyConfig = null) {
+  if (proxyConfig === null) {
+    return toxiproxy.createProxy(proxyBody)
   } else {
-    if (proxyConfig === null) {
-      return toxiproxy.createProxy(proxyBody)
-    } else {
-      return toxiproxy.createProxy(proxyConfig)
-    }
+    return toxiproxy.createProxy(proxyConfig)
   }
 }
 // Latency, Jitter value in ms
-function addLatency (proxy, latencyValue, jitterValue) {
+function addLatency(proxy, latencyValue, jitterValue) {
   const toxicBody = {
-    attributes: {latency: latencyValue, jitter: jitterValue},
+    attributes: { latency: latencyValue, jitter: jitterValue },
     type: 'latency',
-  };
-  proxy.addToxic (new toxiproxyClient.Toxic (proxy, toxicBody));
+  }
+  proxy.addToxic(new toxiproxyClient.Toxic(proxy, toxicBody))
 }
 // Latency value in ms, if 0 the connection won't close,
-function addTimeout (proxy, timeoutValue) {
+function addTimeout(proxy, timeoutValue) {
   const toxicBody = {
-    attributes: {timeout: timeoutValue},
+    attributes: { timeout: timeoutValue },
     type: 'timeout',
-  };
-  proxy.addToxic (new toxiproxyClient.Toxic (proxy, toxicBody));
+  }
+  proxy.addToxic(new toxiproxyClient.Toxic(proxy, toxicBody))
 }
 
 module.exports = {
